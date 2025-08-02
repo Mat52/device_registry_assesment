3fc37ac9f7b6789ec67d82efea3855f5026c9196
Mateusz Łopatkiewicz <mlopatkiewicz@MacBook-Air-Mateusz.local>
2025-08-02 15:20:53 +0200

    Add return device from user system

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class ReturnDeviceFromUser
-  def initialize
-    # TODO
+  def initialize(requesting_user:, serial_number:, from_user:)
+    @requesting_user = requesting_user
+    @serial_number = serial_number
+    @from_user = from_user
   end
 
   def call
-    # TODO
+    unless @requesting_user.id == @from_user.to_i
+      raise ReturningError::Unauthorized
+    end
+    device = Device.find_by(serial_number: @serial_number, user_id: @from_user)
+    if device.nil?
+      raise UnassigningError::AlreadyUnassigned
+    end
+    device.update!(user_id: nil, previous_user_id: @requesting_user.id)
   end
 end
