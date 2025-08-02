001ef42579a6194c5023ef0ed7f87af550f66e68
Mateusz Łopatkiewicz <mlopatkiewicz@MacBook-Air-Mateusz.local>
2025-08-02 12:06:08 +0200

    Fix Device Controller to handle response 422 for Unauthorized access

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,12 +3,18 @@
 class DevicesController < ApplicationController
   before_action :authenticate_user!, only: %i[assign unassign]
   def assign
-    AssignDeviceToUser.new(
+    assigner = AssignDeviceToUser.new(
       requesting_user: @current_user,
       serial_number: params[:serial_number],
       new_device_owner_id: params[:new_device_owner_id]
     ).call
-    head :ok
+
+    if @current_user != params[:new_device_owner_id]
+      render json: { error: 'Unauthorized' }, status: :unprocessable_entity
+    else
+      assigner.call
+      head :ok
+    end
   end
 
   def unassign
@@ -18,6 +24,6 @@ class DevicesController < ApplicationController
   private
 
   def device_params
-    params.permit(:new_owner_id, :serial_number)
+    params.permit(:new_device_owner_id, :serial_number)
   end
 end
