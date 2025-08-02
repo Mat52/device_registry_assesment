8126d235376eef393a978cd135bd4ae6be652eb5
Mateusz Łopatkiewicz <mlopatkiewicz@MacBook-Air-Mateusz.local>
2025-08-02 19:50:55 +0200

    Add some comments to Errors to correctly diagnose the error

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -11,26 +11,37 @@ class ReturnDeviceFromUser
     device = find_device!
     authorize_user!(device)
     active_assignment = find_active_assignment!(device)
+
     ActiveRecord::Base.transaction do
       active_assignment.update!(returned_at: Time.current)
       device.update!(user_id: nil)
     end
+
     :success
   end
 
   private
 
   def find_device!
-    Device.find_by(serial_number: @serial_number) || raise(AssigningError::AlreadyUnassigned)
+    Device.find_by(serial_number: @serial_number) ||
+      raise(AssigningError::DeviceNotFound, "Device with serial #{@serial_number} does not exist")
   end
 
   def authorize_user!(device)
-    raise AssigningError::AlreadyUnassigned unless device.user_id == @from_user
-    raise AssigningError::Unauthorized unless @user.id == @from_user
+    if device.user_id != @from_user
+      raise AssigningError::AlreadyUnassigned,
+            "Device #{@serial_number} is not currently assigned to user #{@from_user}"
+    end
+
+    if @user.id != @from_user
+      raise AssigningError::Unauthorized,
+            "User #{@user.id} is not allowed to return device #{@serial_number} for user #{@from_user}"
+    end
   end
 
   def find_active_assignment!(device)
     device.device_assignments.find_by(user_id: @user.id, returned_at: nil) ||
-      raise(AssigningError::AlreadyUnassigned)
+      raise(AssigningError::AlreadyUnassigned,
+            "No active assignment found for user #{@user.id} on device #{@serial_number}")
   end
 end
