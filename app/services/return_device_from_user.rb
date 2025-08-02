4c5872e74bba9971984f8f180ed7cabddfd8bab3
Mateusz Łopatkiewicz <mlopatkiewicz@MacBook-Air-Mateusz.local>
2025-08-02 18:54:44 +0200

    Tests for returning devices added, code implementation also for handling those tests

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -8,14 +8,12 @@ class ReturnDeviceFromUser
   end
 
   def call
-    raise AssigningError::Unauthorized unless @user.id == @from_user.to_i
-
-    device = Device.find_by(serial_number: @serial_number, user_id: @from_user)
+    device = Device.find_by(serial_number: @serial_number)
     raise AssigningError::AlreadyUnassigned if device.nil?
-
-    active_assignment = device.device_assignments.where(user_id: @user.id, returned_at: nil).last
+    raise AssigningError::AlreadyUnassigned if device.user_id != @from_user.to_i
+    raise AssigningError::Unauthorized if @user.id != @from_user.to_i
+    active_assignment = device.device_assignments.find_by(user_id: @user.id, returned_at: nil)
     raise AssigningError::AlreadyUnassigned if active_assignment.nil?
-
     active_assignment.update!(returned_at: Time.current)
     device.update!(user_id: nil)
   end
--- spec/services/return_device_from_user_spec.rb
+++ spec/services/return_device_from_user_spec.rb
@@ -3,5 +3,66 @@
 require 'rails_helper'
 
 RSpec.describe ReturnDeviceFromUser do
-  # TODO: Implement the tests for ReturnDeviceFromUser
+  let(:user) { create(:user) }
+  let(:serial_number) { '123456' }
+  let(:from_user) { user.id }
+
+  def return_device
+    described_class.new(
+      user: user,
+      serial_number: serial_number,
+      from_user: from_user
+    ).call
+  end
+
+  before do
+    AssignDeviceToUser.new(
+      requesting_user: user,
+      serial_number: serial_number,
+      new_device_owner_id: user.id
+    ).call
+  end
+
+  context 'when the user returns their own active device' do
+    it 'marks the assignment as returned and clears the device owner' do
+      return_device
+
+      device = Device.find_by(serial_number: serial_number)
+      assignment = device.device_assignments.last
+
+      expect(device.user_id).to be_nil
+      expect(assignment.returned_at).not_to be_nil
+    end
+  end
+
+  context 'when the user tries to return a device they never owned' do
+    let(:other_user) { create(:user) }
+    let(:from_user) { other_user.id }
+
+    it 'raises an AlreadyUnassigned error' do
+      expect { return_device }.to raise_error(AssigningError::AlreadyUnassigned)
+    end
+  end
+
+  context 'when the user tries to return an already returned device' do
+    before { return_device }
+
+    it 'raises an AlreadyUnassigned error' do
+      expect { return_device }.to raise_error(AssigningError::AlreadyUnassigned)
+    end
+  end
+
+  context 'when another user tries to return the device' do
+    let(:other_user) { create(:user) }
+
+    it 'raises an Unauthorized error' do
+      expect do
+        described_class.new(
+          user: other_user,
+          serial_number: serial_number,
+          from_user: from_user
+        ).call
+      end.to raise_error(AssigningError::Unauthorized)
+    end
+  end
 end
